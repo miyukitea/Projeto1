714d33970826b83cbdb645e56ce777d524340b04
Miyuki Saito <miyukisaito2@gmail.com>
2025-10-15 08:41:23 -0300

    primeiro commit: estrutura do Weather App

---
+++ script.js
@@ -0,0 +1,21 @@
+async function getWeather() {
+  const city = document.getElementById("city").value;
+  const apiKey = "68ee3a3036c7ccadf99fd4ec5895bafe"; 
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=pt_br&units=metric`;
+
+  const resultDiv = document.getElementById("result");
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error("Cidade não encontrada");
+
+    const data = await response.json();
+    resultDiv.innerHTML = `
+      <p><strong>${data.name}, ${data.sys.country}</strong></p>
+      <p>🌡️ Temperatura: ${data.main.temp}°C</p>
+      <p>☁️ Clima: ${data.weather[0].description}</p>
+    `;
+  } catch (error) {
+    resultDiv.innerHTML = `<p style="color:red">${error.message}</p>`;
+  }
+}
